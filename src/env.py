55cf811a0fbae417bc3b2aa4df94ea9738043220
泠夜Soul <57278745+LingyeSoul@users.noreply.github.com>
2025-06-18 09:19:46 +0800

    修复系统环境实现
    空格路径处理，环境检测方式修改，修复系统环境实现

--- src/env.py
+++ src/env.py
@@ -9,15 +9,13 @@ class Env:
         self.st_dir = os.path.join(os.getcwd(), "SillyTavern\\")
     def checkEnv(self):
         if not os.path.exists(self.base_dir):
-            if  not os.path.exists(self.git_dir):
-                if not os.path.exists(self.node_path):
-                    return True
-                else:
-                    return 'Node path is not exists'
-            else:
-                return 'Git dir is not exists'
-        else:
             return 'Base dir is not exists'
+        if not os.path.exists(self.git_dir):
+            return 'Git dir is not exists'
+        if not os.path.exists(self.node_path):
+            return 'Node path is not exists'
+        return True
+    
     def get_git_path(self):
         return self.git_dir
     def  get_node_path(self):
