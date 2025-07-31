c38060ac7741e6682d53eea3c136308129e5f310
LingyeSoul <57278745+LingyeSoul@users.noreply.github.com>
2025-07-31 21:50:15 +0800

    1.1.0

--- src/ui.py
+++ src/ui.py
@@ -354,7 +354,7 @@ class UniUI():
         ft.Text("关于", size=24, weight=ft.FontWeight.BOLD),
         ft.Divider(),
         ft.Text("SillyTavernLauncher", size=20, weight=ft.FontWeight.BOLD),
-        ft.Text("版本: 1.1.0测试版2", size=16),
+        ft.Text("版本: 1.1.0", size=16),
         ft.Text("作者: 泠夜Soul", size=16),
         ft.ElevatedButton(
             "访问GitHub仓库",
