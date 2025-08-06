7f98d30ffbadf26eb5ceb8ce129b2abf5fa8444a
LingyeSoul <57278745+LingyeSoul@users.noreply.github.com>
2025-08-06 09:50:07 +0800

    1.2.0

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def main(page: ft.Page):
     page.window.min_width=800
     page.window.maximizable = False
     page.window.title_bar_hidden = True
-    version='1.2.0测试版1'
+    version='1.2.0'
     
 
     # 检查是否为首次启动
