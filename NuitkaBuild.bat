966470f1e60485ce66ab31889d53f85b281f9a72
LingyeSoul <57278745+LingyeSoul@users.noreply.github.com>
2025-09-02 23:22:12 +0800

    nuitka支持

---
+++ NuitkaBuild.bat
@@ -0,0 +1,27 @@
+@echo off
+chcp 65001
+set /p SEMVER=请输入版本号（例如 1.0.0）: 
+for /f "tokens=1,2,3 delims=." %%a in ("%SEMVER%") do (
+    set MAJOR=%%a
+    set MINOR=%%b
+    set PATCH=%%c
+)
+
+set APP_NAME=SillyTavernLauncher
+set PRODUCT_NAME=酒馆启动器
+set COMPANY_NAME=LingyeSoul
+set ICON_PATH=src/assets/icon.ico
+set DIST_PATH=Nuitkadist
+
+
+flet packn src/main.py ^
+  --icon "%ICON_PATH%" ^
+  --name "%APP_NAME%" ^
+  --product-name "%PRODUCT_NAME%" ^
+  --file-description "%APP_NAME% %PRODUCT_NAME%" ^
+  --product-version "%SEMVER%" ^
+  --copyright "Copyright (c) 2025 LingyeSoul" ^
+  --distpath "%DIST_PATH%" ^
+  --yes
+
+pause
